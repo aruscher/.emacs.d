7d25bb0bab8c36fd16223b80e1c43bd34ab6fc08
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-10-04 09:35:35 +0200

    add emacs profiler

--- config/40-misc-packages-setup.el
+++ config/40-misc-packages-setup.el
@@ -1,6 +1,7 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+(use-package esup)
 ;; (use-package aggressive-indent
 ;;   :config
 ;;   (global-aggressive-indent-mode t))

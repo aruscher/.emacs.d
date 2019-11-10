20d4f86fa551cf77b71012a10b7af6640491b2b0
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-11-10 16:43:07 +0100

    clean repo

--- config/60-elfeed.el
+++ config/60-elfeed.el
@@ -8,14 +8,3 @@
   (elfeed-org)
   (setq rmh-elfeed-org-files
 	(list "~/.emacs.d/feeds.org")))
-
-(use-package lang-detect
-  :load-path "dev/lang-detect")
-
-(use-package elfeed-lang
-  :after lang-detect
-  :load-path "dev/elfeed-lang")
-
-
-(use-package try
-  :ensure t)

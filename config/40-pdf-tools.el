ca8922343258b7757eeaaa0fdc2bea3c9dd61b9c
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-11-21 23:46:58 +0100

    complete latex setup

--- config/40-pdf-tools.el
+++ config/40-pdf-tools.el
@@ -3,5 +3,9 @@
   :config
   (pdf-tools-install)
   (add-to-list 'auto-mode-alist (cons "\\.pdf$" 'pdf-view-mode))
-  
-  )
+ ;; open pdfs scaled to fit page
+ (setq-default pdf-view-display-size 'fit-page)
+ ;; automatically annotate highlights
+ (setq pdf-annot-activate-created-annotations t)
+ ;; use normal isearch
+ (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))

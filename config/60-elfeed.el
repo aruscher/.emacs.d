e6d2d406c5af63dbacf61aa2a9b0401542823464
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-11-30 13:30:25 +0100

    elfeed dropbox sync

--- config/60-elfeed.el
+++ config/60-elfeed.el
@@ -1,5 +1,21 @@
+(defun bjm/elfeed-load-db-and-open ()
+    (interactive)
+    (elfeed-db-load)
+    (elfeed)
+    (elfeed-search-update--force))
+
+(defun bjm/elfeed-save-db-and-bury ()
+  (interactive)
+  (elfeed-db-save)
+  (quit-window))
+
 (use-package elfeed
-  :ensure t)
+  :ensure t
+  :bind (:map elfeed-search-mode-map
+	      ("q" . bjm/elfeed-save-db-and-bury)
+	      ("Q" . bjm/elfeed-save-db-and-bury))
+  :config
+  (setq elfeed-db-directory "~/Dropbox/shared/elfeeddb"))
 
 (use-package elfeed-org
   :ensure t

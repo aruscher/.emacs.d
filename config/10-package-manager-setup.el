d719df6aebd11e95be2e34c54bc3ddb7d555cb66
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-12-18 08:27:38 +0100

    add package auto update

--- config/10-package-manager-setup.el
+++ config/10-package-manager-setup.el
@@ -8,7 +8,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-
-
 (use-package use-package-ensure-system-package
   :ensure t)
+
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+	 auto-package-update-prompt-before-update t
+         auto-package-update-interval 7)
+   (auto-package-update-maybe))

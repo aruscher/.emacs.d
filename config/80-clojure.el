24bd528aafae9f3fd074a47b0ddfc46fd085579f
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2020-01-20 19:15:55 +0100

    further changes in clojure setup

--- config/80-clojure.el
+++ config/80-clojure.el
@@ -1,3 +1,6 @@
 (use-package cider
-  :ensure t)
+  :ensure t
+  :config (setf cider-lein-command "~/bin/lein")
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'company-mode))
 

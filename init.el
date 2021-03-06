3799de13d764a708f7f8f244417192ad46bcb1e4
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2021-03-06 22:37:39 +0100

    some work

--- init.el
+++ init.el
@@ -1,23 +1,109 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(defalias 'yes-or-no-p 'y-or-n-p) ; Remap yes-or-no to y-or-n
 
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+(defalias 'yes-or-no-p 'y-or-n-p) ; Remap yes-or-no to y-or-n
+(column-number-mode) ; Show line number and column in the minibuffer
+(global-display-line-numbers-mode t) ; Show line numbers on the side
+(global-hl-line-mode t) ; Highlight the current line
+(show-paren-mode t) ; Show matching parens
 
-;; Install org-mode
-(straight-use-package 'org)
+(dolist (mode '(term-mode-hook eshell-mode-hook shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(org-babel-load-file
- (expand-file-name "my-config.org" user-emacs-directory))
+;; Initialize package sources
+(require 'package)
 
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(set-face-attribute 'default nil
+		    :family "Fira Code Retina"
+		    :height 110
+		    :weight 'normal
+		    :width 'normal)
+
+(set-face-attribute 'fixed-pitch nil
+		    :family "Fira Code Retina"
+		    :height 110
+		    :weight 'normal
+		    :width 'normal)
+
+(use-package doom-themes
+  :config (load-theme 'doom-palenight t))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package counsel
+  :diminish
+  :bind (("C-s" . swiper-isearch)
+	 ("M-x" . counsel-M-x)
+	 ("C-h a" . counsel-apropos)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-initial-inputs-alist nil))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package projectile
+  :diminish projectile
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; (when (file-directory-p "~/Code")
+  ;;   (setq projectile-project-search-path '("~/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit projectile helpful ivy-rich zenburn-theme which-key use-package rainbow-delimiters doom-themes doom-modeline counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
